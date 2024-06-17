9192b0c9806c40d044122ca06093bff84fd83263
Ohuegbe01 <praiseohuegbe@gmail.com>
2024-06-17 08:49:12 +0100

    first commit

---
+++ pythonProject1/quiz-game-start/question_model.py
@@ -0,0 +1,7 @@
+# TODO 1. Creating our "Question" class from scratch
+class Question:
+    def __init__(self, text, answer):
+        self.text = text
+        self.answer = answer
+
+# TODO 2. Creating our "question" bank of questions(go to data.py)
